9d06a19681995cf1072a20389763fe808d9dbd86
Yardi Fox <yardi@primeview.com>
2025-03-13 05:12:59 -0700

    update the footer navigation buttons (svg)

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -44,7 +44,8 @@
             </div>
         </div>
     </div>
-    <div class="footer">
+    <div class="footer" id="footer">
+        <button class="rippleBtn" style="background:transparent;">
         <svg class="navBtn" id="navBtnWork" viewBox="266.5,143.5,391,178" xmlns="http://www.w3.org/2000/svg" style="margin-top: -50px;max-width: 160px;cursor: pointer;">
             <g>
                 <path fill="#FF0066" d="M267,144l390,47l-8,98l-356,32z"></path>
@@ -52,9 +53,11 @@
 
             </g>
         </svg>
+        </button>
     </div>
 @endsection
 @section('scripts')
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
     <script type="text/javascript">
         (function(){
             const el = (sel, par) => (par || document).querySelector(sel);
@@ -98,7 +101,11 @@
 
             const scrollToWork = (evt) =>{
                 const element = document.getElementById('contactCont');
-                element.scrollIntoView({ behavior: 'smooth' });
+                // element.scrollIntoView({ behavior: 'smooth' });
+                window.scrollTo({
+                    top: Math.round(element.getBoundingClientRect().top + document.documentElement.scrollTop),
+                    behavior: 'smooth',
+                })
             }
 
             elWrap.addEventListener("mousemove", tilt);
@@ -106,6 +113,111 @@
             navWork.addEventListener("click", scrollToWork);
             console.log('elWrap');
             console.log(elWrap);
+
+            class Button {
+                constructor(HTMLButtonElement) {
+                    this.button = HTMLButtonElement;
+                    this.width = this.button.offsetWidth;
+                    this.height = this.button.offsetHeight;
+                    this.left = this.button.offsetLeft;
+                    this.top = this.button.offsetTop;
+                    this.x = 0;
+                    this.y = 0;
+                    this.cursorX = 0;
+                    this.cursorY = 0;
+                    this.magneticPullX = 0.4;
+                    this.magneticPullY = 0.9;
+                    this.isHovering = false;
+                    this.magnetise();
+                    this.createRipple();
+                }
+
+                onEnter = () => {
+                    gsap.to(this.button, 0.4, {
+                        x: this.x * this.magneticPullX,
+                        y: this.y * this.magneticPullY,
+                        ease: Power4.easeOut
+                    });
+                };
+
+                onLeave = () => {
+                    gsap.to(this.button, 0.7, {
+                        x: 0,
+                        y: 0,
+                        ease: Elastic.easeOut.config(1.1, 0.5)
+                    });
+                };
+
+                magnetise = () => {
+                    document.querySelector("#footer").addEventListener("mousemove", (e) => {
+                        this.cursorX = e.clientX;
+                        this.cursorY = e.offsetY;
+                        console.log("cursorX",e.clientX);
+                        console.log("cursorY",e.offsetY);
+
+                        const center = {
+                            x: this.left + this.width / 2,
+                            y: this.top + this.height / 2
+                        };
+                        console.log('left',this.left);
+                        console.log('top',this.top);
+
+                        this.x = this.cursorX - center.x;
+                        this.y = this.cursorY - center.y;
+                        console.log("center.x",center.x);
+                        console.log("center.y",center.y);
+
+                        const distance = Math.sqrt(this.x * this.x + this.y * this.y);
+                        const hoverArea = this.isHovering ? 0.6 : 0.5;
+
+                        console.log('distance',distance);
+
+                        if (distance < this.width * hoverArea) {
+                            if (!this.isHovering) {
+                                this.isHovering = true;
+                            }
+                            this.onEnter();
+                        } else {
+                            if (this.isHovering) {
+                                this.onLeave();
+                                this.isHovering = false;
+                            }
+                        }
+                    });
+                };
+
+                createRipple = () => {
+                    this.button.addEventListener("click", () => {
+                        const circle = document.createElement("span");
+                        const diameter = Math.max(
+                            this.button.clientWidth,
+                            this.button.clientHeight
+                        );
+                        const radius = diameter / 2;
+
+                        const offsetLeft = this.left + this.x * this.magneticPullX;
+                        const offsetTop = this.top + this.y * this.magneticPullY;
+
+                        circle.style.width = circle.style.height = `${diameter}px`;
+                        circle.style.left = `${this.cursorX - offsetLeft - radius}px`;
+                        circle.style.top = `${this.cursorY - offsetTop - radius}px`;
+                        circle.classList.add("ripple");
+
+                        const ripple = this.button.getElementsByClassName("ripple")[0];
+
+                        if (ripple) {
+                            ripple.remove();
+                        }
+
+                        this.button.appendChild(circle);
+                    });
+                };
+            }
+
+            const buttons = document.getElementsByClassName("rippleBtn");
+            for (const button of buttons) {
+                new Button(button);
+            }
         })()
     </script>
 @endsection
