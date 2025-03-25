8ce3faa82b51f6611214e3cd44e15548bac48243
Yardi Fox <yardi@primeview.com>
2025-03-24 23:47:50 -0700

    updated structured data

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -59,7 +59,7 @@
     </header>
     <div class="wrap">
         <div class="section platformer">
-                (0.0)
+               <div id="ninjaSprite" class="sprite phx">(0.0)</div>
         </div>
         <div class="section attract" id="attractWrap">
             <h1>Test</h1>
@@ -102,8 +102,10 @@
     </div>
 @endsection
 @section('scripts')
-    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script> -->
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+    <script type="text/javascript" src="{{ asset("/js/ninja.js")}}"/>-->
     <script type="text/javascript">
+        let loadButtons = null;
         document.addEventListener('scroll', gsaplaunch);
         document.addEventListener('mousedown', gsaplaunch);
         document.addEventListener('mousemove', gsaplaunch);
@@ -120,6 +122,7 @@
             document.removeEventListener('touchstart', gsaplaunch);
             document.removeEventListener('scroll', gsaplaunch);
             document.removeEventListener('keydown', gsaplaunch);
+            loadButtons();
         }
         let navBtnArray = [];
         (function(){
@@ -219,128 +222,133 @@
             navContact.addEventListener("click", scrollToContact);
             console.log('elWrap');
             console.log(elWrap);
+            loadButtons = ()=>{
 
-            class Button {
-                constructor(HTMLButtonElement) {
-                    this.button = HTMLButtonElement;
-                    this.width = this.button.offsetWidth;
-                    this.height = this.button.offsetHeight;
-                    this.left = this.button.offsetLeft;
-                    this.top = this.button.offsetTop;
-                    this.x = 0;
-                    this.y = 0;
-                    this.center = {};
-                    this.cursorX = 0;
-                    this.cursorY = 0;
-                    this.magneticPullX = 0.4;
-                    this.magneticPullY = 0.9;
-                    this.isHovering = false;
-                    this.magnetise();
-                    this.createRipple();
-                }
+                class Button {
+                    constructor(HTMLButtonElement) {
+                        this.button = HTMLButtonElement;
+                        this.width = this.button.offsetWidth;
+                        this.height = this.button.offsetHeight;
+                        this.left = this.button.offsetLeft;
+                        this.top = this.button.offsetTop;
+                        this.x = 0;
+                        this.y = 0;
+                        this.center = {};
+                        this.cursorX = 0;
+                        this.cursorY = 0;
+                        this.magneticPullX = 0.4;
+                        this.magneticPullY = 0.9;
+                        this.isHovering = false;
+                        this.magnetise();
+                        this.createRipple();
+                    }
 
-                onEnter = () => {
-                    gsap.to(this.button, 0.4, {
-                        x: this.x * this.magneticPullX,
-                        y: this.y * this.magneticPullY,
-                        ease: Power4.easeOut
-                    });
-                };
-
-                onLeave = () => {
-                    gsap.to(this.button, 0.7, {
-                        x: 0,
-                        y: 0,
-                        ease: Elastic.easeOut.config(1.1, 0.5)
-                    });
-                };
-
-                resetCenter = (e) =>{
-                   this.center = {
-                        x: this.button.offsetLeft + this.width / 2,
-                        y: this.button.offsetTop + this.height / 2
+                    onEnter = () => {
+                        gsap.to(this.button, 0.4, {
+                            x: this.x * this.magneticPullX,
+                            y: this.y * this.magneticPullY,
+                            ease: Power4.easeOut
+                        });
                     };
-                   this.left = this.button.offsetLeft;
-                   this.top = this.button.offsetTop;
-                   console.log('this.left: ', this.button.offsetLeft);
-                   console.log('this.top: ', this.button.offsetTop);
-                   console.log(this.center);
-                }
-                magnetise = () => {
-                    document.querySelector("body").removeEventListener("mousemove",this.magnetise);
-                    document.querySelector("body").addEventListener("mousemove", (e) => {
-                        this.cursorX = e.clientX;
-                        this.cursorY = e.offsetY;
-                        // console.log("cursorX",e.clientX);
-                        // console.log("cursorY",e.offsetY);
 
+                    onLeave = () => {
+                        gsap.to(this.button, 0.7, {
+                            x: 0,
+                            y: 0,
+                            ease: Elastic.easeOut.config(1.1, 0.5)
+                        });
+                    };
+
+                    resetCenter = (e) =>{
                         this.center = {
-                            x: this.left + this.width / 2,
-                            y: this.top + this.height / 2
+                            x: this.button.offsetLeft + this.width / 2,
+                            y: this.button.offsetTop + this.height / 2
                         };
-                        // console.log('left',this.left);
-                        // console.log('top',this.top);
+                        this.left = this.button.offsetLeft;
+                        this.top = this.button.offsetTop;
+                        console.log('this.left: ', this.button.offsetLeft);
+                        console.log('this.top: ', this.button.offsetTop);
+                        console.log(this.center);
+                    }
+                    magnetise = () => {
+                        document.querySelector("body").removeEventListener("mousemove",this.magnetise);
+                        document.querySelector("body").addEventListener("mousemove", (e) => {
+                            this.cursorX = e.clientX;
+                            this.cursorY = e.offsetY;
+                            // console.log("cursorX",e.clientX);
+                            // console.log("cursorY",e.offsetY);
 
-                        this.x = this.cursorX - this.center.x;
-                        this.y = this.cursorY -  this.center.y;
-                        // console.log("center.x",center.x);
-                        // console.log("center.y",center.y);
+                            this.center = {
+                                x: this.left + this.width / 2,
+                                y: this.top + this.height / 2
+                            };
+                            // console.log('left',this.left);
+                            // console.log('top',this.top);
 
-                        const distance = Math.sqrt(this.x * this.x + this.y * this.y);
-                        const hoverArea = this.isHovering ? 0.6 : 0.5;
+                            this.x = this.cursorX - this.center.x;
+                            this.y = this.cursorY -  this.center.y;
+                            // console.log("center.x",center.x);
+                            // console.log("center.y",center.y);
 
-                        // console.log('distance',distance);
+                            const distance = Math.sqrt(this.x * this.x + this.y * this.y);
+                            const hoverArea = this.isHovering ? 0.6 : 0.5;
 
-                        if (distance < this.width * hoverArea) {
-                            if (!this.isHovering) {
-                                this.isHovering = true;
+                            // console.log('distance',distance);
+
+                            if (distance < this.width * hoverArea) {
+                                if (!this.isHovering) {
+                                    this.isHovering = true;
+                                }
+                                this.onEnter();
+                            } else {
+                                if (this.isHovering) {
+                                    this.onLeave();
+                                    this.isHovering = false;
+                                }
                             }
-                            this.onEnter();
-                        } else {
-                            if (this.isHovering) {
-                                this.onLeave();
-                                this.isHovering = false;
+                        });
+                    };
+
+                    createRipple = () => {
+                        this.button.addEventListener("click", () => {
+                            const circle = document.createElement("span");
+                            const diameter = Math.max(
+                                this.button.clientWidth,
+                                this.button.clientHeight
+                            );
+                            const radius = diameter / 2;
+
+                            const offsetLeft = this.left + this.x * this.magneticPullX;
+                            const offsetTop = this.top + this.y * this.magneticPullY;
+
+                            circle.style.width = circle.style.height = `${diameter}px`;
+                            circle.style.left = `${this.cursorX - offsetLeft - radius}px`;
+                            circle.style.top = `${this.cursorY - offsetTop - radius}px`;
+                            circle.classList.add("ripple");
+
+                            const ripple = this.button.getElementsByClassName("ripple")[0];
+
+                            if (ripple) {
+                                ripple.remove();
                             }
-                        }
-                    });
-                };
 
-                createRipple = () => {
-                    this.button.addEventListener("click", () => {
-                        const circle = document.createElement("span");
-                        const diameter = Math.max(
-                            this.button.clientWidth,
-                            this.button.clientHeight
-                        );
-                        const radius = diameter / 2;
+                            this.button.appendChild(circle);
+                        });
+                    };
+                }
 
-                        const offsetLeft = this.left + this.x * this.magneticPullX;
-                        const offsetTop = this.top + this.y * this.magneticPullY;
-
-                        circle.style.width = circle.style.height = `${diameter}px`;
-                        circle.style.left = `${this.cursorX - offsetLeft - radius}px`;
-                        circle.style.top = `${this.cursorY - offsetTop - radius}px`;
-                        circle.classList.add("ripple");
-
-                        const ripple = this.button.getElementsByClassName("ripple")[0];
-
-                        if (ripple) {
-                            ripple.remove();
-                        }
-
-                        this.button.appendChild(circle);
-                    });
-                };
-            }
-
-            const buttons = document.getElementsByClassName("rippleBtn");
-            for (const button of buttons) {
-                const btnObj = new Button(button);
-                navBtnArray.push( btnObj );
-                console.log(navBtnArray);
+                const buttons = document.getElementsByClassName("rippleBtn");
+                for (const button of buttons) {
+                    const btnObj = new Button(button);
+                    navBtnArray.push( btnObj );
+                    console.log(navBtnArray);
+                }
             }
 
         })()
+        function setupButtons(){
+
+        }
         window.addEventListener('resize', (e)=>{
             for (const button of navBtnArray) {
                 button.resetCenter(e);
@@ -348,5 +356,55 @@
                 console.log('button',button);
             }
         });
+
+        (function(){
+
+            console.log('ninja.js loaded');
+            let ninja = document.querySelector('#ninjaSprite');
+            ninja.style.position = "absolute";
+
+            let sx = 0;
+            let sy = 0;
+            let vxl = 0;
+            let vxr = 0;
+            let vy = 0;
+            let spriteHeight = ninja.offsetHeight;
+
+            function update(){
+                sx +=  vxl;
+                sx += vxr;
+                sy += vy;
+                if ( sy  + spriteHeight  >=(window.innerHeight + Math.round(window.scrollY)) ){
+                    sy = (window.innerHeight + Math.round(window.scrollY) - spriteHeight );
+                    vy = 0;
+                }
+                console.log((window.innerHeight + Math.round(window.scrollY)));
+                console.log(sy + spriteHeight);
+
+                ninja.style.left = sx + 'px';
+                ninja.style.top = sy+ 'px';
+
+                requestAnimationFrame(update);
+
+            }
+            update();
+            let lastScroll = 0;
+            window.addEventListener("scroll", () =>{
+                let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+
+                if (currentScroll > 0 && lastScroll <= currentScroll){
+                    lastScroll = currentScroll;
+                    scrollPosY = (window.scrollY / document.body.clientHeight);
+                    sy -= scrollPosY*9;
+                }else{
+                    lastScroll = currentScroll;
+                    //document.getElementById("scrollLoc").innerHTML = "Scrolling UP";
+                }
+            })
+            setInterval(function Gravity(){
+                vy += 0.09;
+            },20)
+
+        })();
     </script>
 @endsection
