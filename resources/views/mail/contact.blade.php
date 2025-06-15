35fcf13d66a6c3b0fb81b669148b1c9c2e916cdb
Yardi Fox <yardi@primeview.com>
2025-06-14 17:41:01 -0700

    update the main blade template with contact form js

--- resources/views/mail/contact.blade.php
+++ resources/views/mail/contact.blade.php
@@ -1,5 +1,2 @@
 <h2>Contact Form Submission</h2>
-<p><strong>Name:</strong> {{$name }}</p>
-<p><strong>Email:</strong> {{ $email }}</p>
-<p><strong>Message:</strong></p>
-<p>{{ $message }}</p>
+{{$content}}
