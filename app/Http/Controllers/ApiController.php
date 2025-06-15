d968110069d8170dcdd69f962cc88803dd5fe743
Yardi Fox <yardi@primeview.com>
2025-06-14 18:41:01 -0700

    added reCaptcha to Contact form

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Mail\Message;
+use Illuminate\Http;
 
 class ApiController extends Controller
 {
