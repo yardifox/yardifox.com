e944e4dd715ee0a3f264608d3e7b4195ea7f8fd7
Yardi Fox <yardi@primeview.com>
2025-06-17 03:18:12 -0700

    css update

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -2,7 +2,7 @@
 @section('head')
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
     <script type="application/ld+json">
         {
             "@context": "https://schema.org",
