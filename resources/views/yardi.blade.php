5a046d0fc5a101ff2c8b6530b7be8f25aa8626dd
Yardi Fox <yardi@primeview.com>
2025-06-17 04:15:48 -0700

    css tweaks

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -458,7 +458,7 @@
                 const rotateY = extractRotateYFromMatrix3D(pTransform);
                 let pWidth = parseInt(paneStyle.width);
                 //paneShadow.style.transform = rotateY ? `rotateX(${rotateY.toFixed(2)}deg`: '';
-                paneShadow.style.transform = dampenMatrix3D(pTransform,0.12);
+                paneShadow.style.transform = dampenMatrix3D(pTransform,0.23);
                 paneShadow.style.width = rotateY ? (pWidth  -(Math.abs(rotateY) * 3.23))+'px' : pWidth+'px';
 
                 requestAnimationFrame(update);
