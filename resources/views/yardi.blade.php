62f5869764d1ba4934debbc99ca3701c90f29d52
Yardi Fox <yardi@primeview.com>
2025-06-17 03:11:57 -0700

    updated attract section

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.main')
 @section('head')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
     <script type="application/ld+json">
         {
             "@context": "https://schema.org",
