9ecea725d3c0b6a5782cf43d783303ad8d117113
Yardi Fox <yardi@primeview.com>
2025-06-22 13:57:19 -0700

    update the tilt touch handle to update the about text

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -303,7 +303,7 @@
                 elTextTilt.style.textShadow = `
                     ${settings.axis === "x" ? 0 :-tiltX*1.9}px ${settings.axis === "y" ? 0 :tiltY*1.9}px 1px rgba(0,0,0,0.5)
                 `;
-                const h2ShadowValue = `drop-shadow(${settings.axis === "x" ? 0 : -tiltX * 1.9}px ${settings.axis === "y" ? 0 : tiltY * 1.9}px 2px #000000)`;
+                const h2ShadowValue = `drop-shadow(${settings.axis === "x" ? 0 : -tiltX * 1.9}px ${settings.axis === "y" ? 0 : tiltY * 1.9}px 1px rgba(0,0,0,0.5))`;
                 h2TextTilt.style.webkitFilter  = h2ShadowValue;
                 h2TextTilt.style.filter  = h2ShadowValue;
                 console.log('h2TextTilt');
