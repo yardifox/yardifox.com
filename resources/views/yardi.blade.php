b751fe15e0622e440047ff3d51e4186979c6d51a
Yardi Fox <yardi@primeview.com>
2025-06-22 01:26:55 -0700

    defered Three.js code

--- resources/views/yardi.blade.php
+++ resources/views/yardi.blade.php
@@ -292,8 +292,8 @@
                 scale(${settings.scale})
               `;
                 elTextTilt.style.transform = `
-                translateX(${settings.axis === "x" ? 0 : textTiltY}deg)
-                translateY(${settings.axis === "y" ? 0 : textTiltX}deg)
+                translateX(${settings.axis === "x" ? 0 : textTiltY}px)
+                translateY(${settings.axis === "y" ? 0 : textTiltX}px)
                 scale(${settings.scale})
                 `;
             }
